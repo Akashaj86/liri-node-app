27e176f2a383b6d6a8ec3f946b498b4db16c2bb7
Ashlie Johnson <ashlie86.2003@yahoo.com>
2019-09-27 20:16:45 -0400

    finished app with video in readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # liri-node-app
 Week 10 node js app
 
-This is an app to call data about songs and movies, the user makes a request for info in the terminal, which returns information about that request (i.e. movie/artist/song).
+This is an app to call data about songs and movies. The user makes a request for info in the terminal, which returns information about that request (i.e. movie/artist/song).
+
+Below is a video link showing a working Spotify data request.
+
+https://drive.google.com/file/d/1b-sx9XxJ5QkRH1j1-lRaFv5HQMgyEI1h/view
--- liri.js
+++ liri.js
@@ -19,7 +19,7 @@ var bandsintown = (keys.bandsintown);
 
 //takes user command & input
 var userInput = process.argv[2];
-var userQuery = process.argv.slice(3).joint(" ");
+var userQuery = process.argv.slice(3).join(" ");
 
 //API Logic here
 function userCommand(userInput, userQuery) {
@@ -29,7 +29,7 @@ function userCommand(userInput, userQuery) {
             break;
 
         case "spotify-this":
-            spotifyThisSong();
+            spotifyThisSong(userQuery);
             break;
 
         case "movie-this":
@@ -43,13 +43,13 @@ function userCommand(userInput, userQuery) {
         default:
             console.log("I don't understand");
             break
-    }
-}
+    };
+};
 userCommand(userInput, userQuery);
 
 function concertThis() {
     console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
-    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, response, body); {
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
             var userBand = JSON.parse.apply(body);
@@ -67,15 +67,15 @@ function concertThis() {
             } else {
                 console.log('Band or concert not found!');
             };
-        }
-    }
+        };
+    });
 };
 
-function spotifyThisSong() {
+function spotifyThisSong(userQuery) {
     console.log(`\n -----\n\nSEARCHING FOR..."${userQuery}"`);
 
     //if user query not found, pass not found
-    if (!userQuery) { userQuery = "Not Found" };
+    if (!userQuery) { userQuery = "The Sign Ace of Base" };
 
     //spotify search query format
     spotify.search({ type: 'track', query: userQuery, limit: 1 }, function (error, data) {
@@ -84,10 +84,22 @@ function spotifyThisSong() {
         }
         //collect selected data into an array
         var spotifyArr = data.tracks.items;
+        var preview;
+        if (!spotifyArr[0].preview_url) { 
+            preview = "Sorry, no preview available" 
+        } else {
+            preview = spotifyArr[0].preview_url
+        }
+       // console.log(data.tracks.items[0]);
+        console.log(spotifyArr[0].name);
+        console.log(spotifyArr[0].artists[0].name);
+        console.log(spotifyArr[0].album.name);
+        console.log(preview);
 
-        for (i = 0; i, spotifyArr.length; i++) {
-            console.length(`\nBA DA Bop! That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\n\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\nAlbum: ${data.tracks.items[i].album.name}\n\n -----`)
-        };
+
+        // for (i = 0; i, spotifyArr.length; i++) {
+        //     console.log(`\nBA DA Bop! That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\n\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\nAlbum: ${data.tracks.items[i].album.name}\n\n -----`)
+        // };
     })
 };
 
@@ -97,20 +109,25 @@ function moviethis() {
 
     //requesting & using omdb api
     var omdb = process.env.OMDB_KEY
-    request("http://www.omdbapi.com/?tm" + userQuery + "&apikey=86fe999c", function (error, response, body));
-    var userMovie = JSON.parse(body);
+    request("http://www.omdbapi.com/?tm" + userQuery + "&apikey=86fe999c", function (error, response, body) {
+        var userMovie = JSON.parse(body);
+        console.log(userMovie);
+        //captured nested ratings in an array
+        var ratingsArr = userMovie.Ratings;
+        // if (ratingsArr.length === 2) {
+        //   console.log("hello", ratingsArr);
+        // }
 
-    //captured nested ratings in an array
-    var ratingsArr = userMovie.Ratings;
-    if (ratingsArr.length.2) {
-    }
+        if (!error && response.statusCode === 200) {
+            console.log(`\nBA DA Bop! that's for you... \n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDB Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratins[i].Value}\nCountry: ${userMovieCountry}\nLanguage: ${userMovie.language}\nPlot: ${userMovie.plot}\n\n-----`)
+        } else {
+            return console.log("Movie able to be found. Error: " + error)
+        };
+    });
 
-    if (!error && response.statusCode === 200) {
-        console.log(`\nBA DA Bop! that's for you... \n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDB Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratins[i].Value}\nCountry: ${userMovieCountry}\nLanguage: ${userMovie.language}\nPlot: ${userMovie.plot}\n\n-----`)
-    } else {
-        return console.log("Movie able to be found. Error: " + error)
-    };
 };
+
+//moviethis();
 
 function doThis() {
     //utilizing built-in readme to access random.txt
